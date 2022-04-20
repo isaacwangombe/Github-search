59b99dae0d496f01fe6abbcd961f68fd1516586b
isaac mariga <isaacwangombe5@gmail.com>
2022-04-20 06:05:10 +0300

    realighnong routing

--- src/app/components/repository/repository.component.ts
+++ src/app/components/repository/repository.component.ts
@@ -11,10 +11,23 @@ export class RepositoryComponent implements OnInit {
   repos:any;
   profile:any;
   userName:string;
+  user: Object;
+  
 
   constructor(
-    private userService:UserService, private repositoryService:RepositoryService
-  ) { }
+    private userService: UserService, private repositoryService: RepositoryService
+
+  ) {
+    this.userService.getUsers().subscribe(user => {
+      this.user = user;
+    });
+
+    this.userService.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+
+  }
   findRepo() {
     this.repositoryService.updateUser(this.userName);
     this.repositoryService.getRepoInformation()
